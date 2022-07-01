8ad277b288b16c121262b4358a9697bffcd011a4
Akash Das <akashd595@gmail.com>
2022-07-01 20:54:21 +0530

    ADDED: Prime Factors

---
+++ Factors.java
@@ -0,0 +1,19 @@
+package com.core_java_problems;
+
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number ");
+        int num = sc.nextInt();
+
+        int temp = num;
+        for (int i=2; i*i<=temp; i++){
+            while(num%i == 0){
+                System.out.println(i);
+                num = num/i;
+            }
+        }
+    }
+}
